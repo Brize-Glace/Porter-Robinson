48940945f3cbba46c8c6d1ad00ceb12c25154613
Brize Glace <viktorovitchromain@gmail.com>
2025-02-09 23:37:30 +0100

    :sparkles: v1.1.0: feat(concerts): add 'Sold Out' badge for sold out concerts
    Improve user experience by clearly indicating sold out concerts.
    
    - added a 'Sold Out' badge to mark concerts that are fully booked

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,8 @@
 fetch('public/json/concerts.json')
-  .then(response => response.json())
+  .then(response =>  {
+    if (!response.ok) throw new Error("Erreur HTTP : " + response.status);
+    return response.json();
+  })
   .then(data => {
     let concerts = data.concerts.filter(concert => new Date(concert.date) > new Date());
     
@@ -27,13 +30,27 @@ fetch('public/json/concerts.json')
 
     updateCountdown(chosenConcert.date);
 
+    
+    if (chosenConcert.soldOut) {
+      document.querySelector('.description').textContent += " Sold out!";
+    }
+
     select.addEventListener('change', () => {
       const selectedCity = select.value;
       const chosenConcert = concerts.find(c => c.city === selectedCity);
       document.querySelector('.description').textContent = chosenConcert.location;
+
+      if (chosenConcert.soldOut) {
+        document.querySelector('.description').innerHTML += "<span>Sold out!</span>";
+      }
       updateCountdown(chosenConcert.date);
     });
-  });
+  })
+  .catch(error => {
+    console.error("Erreur :", error);
+    document.querySelector('.heading').textContent = "Unable to load concerts";
+    document.querySelector('.description').innerHTML = `Please try again later -  You can open an issue at <a href="https://github.com/Brize-Glace/Porter-Robinson/issues">here</a>`;
+});
 
 let countdownInterval;
 
